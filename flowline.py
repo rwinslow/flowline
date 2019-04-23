e279822eaff690f94e2a18572c3a403391b06d0f
Rich Winslow <rich@richwinslow.com>
2019-04-23 00:59:50 -0700

    allow kwargs to be set to flowline instance on run

--- flowline.py
+++ flowline.py
@@ -2,22 +2,29 @@ import inspect
 
 
 class Flowline(object):
+
     def __init__(self, name, tasks=tuple(), **kwargs):
         self.name = name
         self.tasks = tasks
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.self_arg = self.__class__.__name__.lower()
+        self.save_kwargs(kwargs)
 
-    def run(self, context):
-        for context in self.walk(context):
+    def run(self, context, **kwargs):
+        self.save_kwargs(kwargs)
+        result = None
+        for result in self.walk(context):
             pass
-        return context
+        return result
 
     def walk(self, context):
         for task in self.tasks:
             if inspect.isfunction(task):
-                if 'flowline' in inspect.signature(task).parameters:
+                if self.self_arg in inspect.signature(task).parameters:
                     context = task(context, flowline=self)
                 else:
                     context = task(context)
         yield context
+
+    def save_kwargs(self, kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
