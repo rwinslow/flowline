f494c2d63fe7a384cf6eeded07376049a91f0bd4
Rich Winslow <rich@richwinslow.com>
2019-04-23 23:42:29 -0700

    replace distutils with setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='flowline',
     version='0.0.1',
-    description='Python workflow engine',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author='Rich Winslow',
     author_email='rich@richwinslow.com',
     url='https://github.com/rwinslow/flowline',
