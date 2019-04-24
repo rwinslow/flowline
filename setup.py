91a92677f69e284624f25d4e8410d1a85a56aa18
Rich Winslow <rich@richwinslow.com>
2019-04-23 23:38:56 -0700

    add setup and build functionality

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='flowline',
+    version='0.0.1',
+    description='Python workflow engine',
+    author='Rich Winslow',
+    author_email='rich@richwinslow.com',
+    url='https://github.com/rwinslow/flowline',
+    packages=['flowline'],
+)
